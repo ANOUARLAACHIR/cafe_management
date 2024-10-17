59834dbdbc0615046400bfa76cf5e9972f86d1f1
Anouar LAACHIR <become.engineer88@gmail.com>
2024-10-17 19:54:16 +0100

    declaring UserServiceImpl as @Service and UserRestImpl as @RestController

--- src/main/java/com/codewa/cafe/impl/UserRestImpl.java
+++ src/main/java/com/codewa/cafe/impl/UserRestImpl.java
@@ -1,6 +1,8 @@
 package com.codewa.cafe.impl;
 
 import com.codewa.cafe.rest.UserRest;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UserRestImpl implements UserRest {
 }
--- src/main/java/com/codewa/cafe/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/codewa/cafe/serviceImpl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.codewa.cafe.serviceImpl;
 
 import com.codewa.cafe.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 }

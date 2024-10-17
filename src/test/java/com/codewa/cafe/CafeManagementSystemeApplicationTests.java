5ebc0bef91eb415a6d76c0d8378641ff372ba77f
Anouar LAACHIR <become.engineer88@gmail.com>
2024-10-17 19:19:59 +0100

    initial

---
+++ src/test/java/com/codewa/cafe/CafeManagementSystemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewa.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

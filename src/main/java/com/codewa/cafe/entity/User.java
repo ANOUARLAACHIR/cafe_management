14dab50e5c0a2373f681f93f2b4391511c177417
Anouar LAACHIR <become.engineer88@gmail.com>
2024-10-17 19:45:06 +0100

    make User class Serializable

--- src/main/java/com/codewa/cafe/entity/User.java
+++ src/main/java/com/codewa/cafe/entity/User.java
@@ -10,10 +10,16 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 @DynamicInsert @DynamicUpdate
-public class User {
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

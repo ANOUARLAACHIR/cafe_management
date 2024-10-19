07ef1f3910e87dafb33c2aed438a3252b4f472c2
Anouar LAACHIR <become.engineer88@gmail.com>
2024-10-19 22:10:12 +0100

    create methods extractUsername, extractExpiration, isTokenExpired and validateToken

---
+++ src/main/java/com/codewa/cafe/JWT/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.codewa.cafe.JWT;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+    private final String SECRET = "codewithanouar";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith(getSigninKey())
+                .build().parseSignedClaims(token).getPayload();
+    }
+
+    private SecretKey getSigninKey() {
+        byte[] bytes = SECRET.getBytes();
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
